272af0e5d680c2682cf8b8b0b0db825d1cb7670e
Vadim <vadimgol07@inbox.ru>
2024-04-30 14:25:18 +0300

    внесены изменения в тип возвращаемой переменной

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,7 @@
 public class BmiService {
     public double calculate(int weight, double height) {
         double index = weight / (height * height); //формула для расчёта ИМТ
+        //int indexResult = (int) index; //приводит к целому числу
         return index; //возвращаем рассчитанный ИМТ
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int weight = 89; //вес в кг
-        double height = 1.87; //рост в метрах
-        double index = service.calculate(weight, height); //считаем ИМТ
-        int indexResult = (int) index; //приводи к целому числу
+        int weight = 48; //вес в кг
+        double height = 1.78; //рост в метрах
+        int index = (int) service.calculate(weight, height); //считаем ИМТ и отбрасывает дробь
+        //int indexResult = (int) index; //приводит к целому числу
         //String indexResult = String.format("%.0f", index); // или так округлить приводит к целому числу
 
         System.out.println("Ваш вес: " + weight + " кг.");
         System.out.println("Ваш рост: " + height + " м.");
-        System.out.println("Ваш индекс массы тела: " + indexResult);
+        System.out.println("Ваш индекс массы тела: " + index);
     }
 }
