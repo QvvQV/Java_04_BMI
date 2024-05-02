0b372da0ace30cebc869b66f39c791a804554e03
Vadim <vadimgol07@inbox.ru>
2024-05-02 19:56:13 +0300

    type 'calculate' change with 'double' for 'int'

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
-    public double calculate(int weight, double height) {
+    public int calculate(int weight, double height) {
         double index = weight / (height * height); //формула для расчёта ИМТ
-        //int indexResult = (int) index; //приводит к целому числу
-        return index; //возвращаем рассчитанный ИМТ
+        int indexResult = (int) index; //приводит к целому числу
+        return indexResult; //возвращаем рассчитанный ИМТ
     }
 }
